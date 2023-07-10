d823c4402f0d42bce43fcd920dcad98ce5b4675f
hicham <hichambtphicham@gmail.com>
2023-07-10 10:44:11 -0700

    up

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-
+#ifndef FILE_MAIN
+#define FILE_MAIN
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,4 +21,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif
