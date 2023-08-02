b8c9c78f6040d99aa34e9e10f8df6cf3a7ec83a2
hichamle <132504659+hichamle@users.noreply.github.com>
2023-08-02 13:38:42 +0100

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,18 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+13:37
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - prints its own opcodes
  * @argc: number of arguments
@@ -12,23 +24,18 @@ int main(int argc, char *argv[])
 {
 	int bytes, i;
 	char *arr;
-
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
 	bytes = atoi(argv[1]);
-
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
 	arr = (char *)main;
-
 	for (i = 0; i < bytes; i++)
 	{
 		if (i == bytes - 1)
@@ -38,9 +45,5 @@ int main(int argc, char *argv[])
 		}
 		printf("%02hhx ", arr[i]);
 	}
-	return (0);
-}		}
-	}
-
 	return (0);
 }
